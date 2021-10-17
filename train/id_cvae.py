ba8659dd2cf70fac03ddc9b87febc952885e3d9c
再提出 <quietsato@gmail.com>
2021-10-17 23:06:55 +0900

    Cleanup code

--- train/id_cvae.py
+++ train/id_cvae.py
@@ -1,24 +1,18 @@
-#%%
-from posixpath import basename, dirname
-from torch import nn, Tensor
+import torch
+from torch import nn
 from torchvision import datasets, transforms
 from torchvision.datasets.mnist import MNIST
-from torchvision.transforms.transforms import Normalize
 
-import matplotlib.pyplot as plt
 import os
 
-#%%
-mnist: MNIST = datasets.MNIST(
-    root=os.path.join(dirname(__file__), '..', 'data'),
+mnist_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Lambda(lambda x: (x + 1.) / 2.),
+])
+
+train_dataset: MNIST = datasets.MNIST(
+    root=os.path.join(os.path.dirname(__file__), '..', 'data'),
     train=True,
-    transform=[transforms.ToTensor()],
+    transform=mnist_transforms,
     download=True,
 )
-
-#%%
-print(mnist.data.shape)
-plt.imshow(mnist.data[0], cmap='gray')
-plt.axis(False)
-
-# %%
