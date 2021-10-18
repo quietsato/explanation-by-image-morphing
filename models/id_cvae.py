fd551ba4534fab676e956a465c1810ca16027e96
再提出 <quietsato@gmail.com>
2021-10-18 15:03:24 +0900

    Implement encoder

--- models/id_cvae.py
+++ models/id_cvae.py
@@ -1,8 +1,62 @@
+from typing import List
 import torch
+from torch import Tensor, nn
 
-class ID_CVAE(torch.nn.Module):
+
+class Encoder(torch.nn.Module):
+    def __init__(self,
+                 image_size: int,
+                 image_channels: int,
+                 latent_dim: int,
+                 conv_out_channels: List[int],
+                 conv_kernel_size: List[int],
+                 conv_stride: List[int]):
+        super().__init__()
+
+        self.seq = nn.Sequential()
+        n = min(len(conv_out_channels), len(conv_kernel_size), len(conv_stride))
+        for i in range(n):
+            c_in = conv_out_channels[i-1] if i > 0 else image_channels
+            c_out = conv_out_channels[i]
+            ck = conv_kernel_size[i]
+            cs = conv_stride[i]
+            image_size //= cs
+            self.seq.add_module(f"conv2d_{i+1}", nn.Conv2d(c_in, c_out, ck, cs, padding=(ck-1)//2))
+            self.seq.add_module(f"relu", nn.ReLU())
+        linear_in = image_size * image_size * conv_out_channels[n-1]
+        self.seq.add_module(f"flatten", nn.Flatten())
+
+        self.W_z_mean = nn.Linear(linear_in, latent_dim)
+        self.W_log_var = nn.Linear(linear_in, latent_dim)
+
+    def forward(self, x: Tensor):
+        f = self.seq(x)
+
+        z_mean: Tensor = self.W_z_mean(f)
+        z_log_var: Tensor = self.W_log_var(f)
+        epsilon = torch.randn(list(z_mean.size())[:2])
+
+        z = z_mean + torch.exp(.5 * z_log_var) * epsilon
+
+        return z, z_mean, z_log_var
+
+
+class Decoder(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward():
+    def forward(self, z: Tensor, y: Tensor):
         pass
+
+
+class ID_CVAE(torch.nn.Module):
+    def __init__(self, E: Encoder, D: Decoder):
+        super().__init__()
+
+        self.E = E
+        self.D = D
+
+    def forward(self, x: Tensor, y: Tensor):
+        z: Tensor = self.E(x)[0]
+        x_ = self.D(z, y)
+        return x_
---
+++ tests/test_id_cvae.py
@@ -0,0 +1,63 @@
+import unittest
+import torch
+from torch import Tensor
+
+
+class ID_CVAETest(unittest.TestCase):
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName=methodName)
+        self.batch_size = 8
+        self.image_size = 28
+        self.image_channels = 1
+        self.num_classes = 10
+        self.latent_dim = 2
+
+    def test_encoder(self):
+        E = Encoder(
+            self.image_size,
+            self.image_channels,
+            self.latent_dim,
+            conv_out_channels=[4, 8, 8],
+            conv_kernel_size=[4, 4, 3],
+            conv_stride=[1, 2, 2]
+        )
+
+        x = torch.randn([
+            self.batch_size,
+            self.image_channels,
+            self.image_size,
+            self.image_size])
+
+        z, z_mean, z_log_var = E(x)
+
+        self.assertEqual(list(z.shape), [self.batch_size, self.latent_dim])
+        self.assertEqual(list(z_mean.shape), [self.batch_size, self.latent_dim])
+        self.assertEqual(list(z_log_var.shape), [self.batch_size, self.latent_dim])
+
+    def test_decoder(self):
+        D = Decoder(
+            self.image_size,
+            self.image_channels,
+            self.num_classes,
+            conv_out_channels=[4, 8, 8],
+            conv_kernel_size=[4, 4, 4],
+            pool_kernel_size=[1, 2, 2]
+        )
+
+        z = torch.zeros([
+            self.batch_size,
+            self.latent_dim
+        ])
+
+        x: Tensor = D(z)
+
+        self.assertEqual(list(x.shape), [self.batch_size, self.num_classes])
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+    from models.id_cvae import *
+
+    unittest.main()
