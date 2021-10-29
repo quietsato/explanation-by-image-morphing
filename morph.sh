8e60fd044fc05c010272409166e58b1198a6300d
再提出 <quietsato@gmail.com>
2021-10-29 16:41:46 +0900

    Concat videos

--- morph.sh
+++ morph.sh
@@ -4,8 +4,13 @@ DIFFMORPH_DIR="./DiffMorph"
 
 IMAGE_DIR="../out/main/20211029155602_test0_morphing_idcvae_only"
 MORPH_NUM=10
+CONCAT_LIST="./morph/concat_list.txt"
 
 EPOCHS=5
+
+cd $DIFFMORPH_DIR
+
+rm -f $CONCAT_LIST
 
 for i in $(seq -f "%03g" $MORPH_NUM); do
     j=`echo "$i - 1" | bc | xargs printf "%03d"`
@@ -13,9 +18,9 @@ for i in $(seq -f "%03g" $MORPH_NUM); do
     MORPH_END="$IMAGE_DIR/$i.png"
     echo $MORPH_START $MORPH_END
 
-    EXEC_DIR=$(pwd)
-    cd $DIFFMORPH_DIR
     python3 ./morph.py -s $MORPH_START -t $MORPH_END -e $EPOCHS
     mv ./morph/morph.mp4 ./morph/$i.mp4
-    cd $EXEC_DIR
+    echo "file './$i.mp4'" >> $CONCAT_LIST
 done
+
+ffmpeg -safe 0 -f concat -i $CONCAT_LIST -c copy ./morph/concatenated.mp4
